f59acfbd291c314f226158e215df9af907b5b9f3
jpsoul-dev <jpsoul-dev@gmail.com>
2023-10-26 12:12:52 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
+import Image from 'next/image'
 import type { AppProps } from 'next/app'
 
+import { Roboto } from 'next/font/google'
+
+import logoImg from '../assets/logo.svg'
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: 'normal',
+  subsets: ['latin'],
+})
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" style={{ height: '5.2rem' }} />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,8 @@
+import { styled } from '..'
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  gap: '4.8rem',
+})
+
+export const Product = styled('a', {})
